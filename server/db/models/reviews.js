d627a23fae4f0d9283c14853902ba6a0f31ee1e3
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2016-04-19 11:33:08 -0400

    reviews, subscriptions models built

---
+++ server/db/models/reviews.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    movie: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movies',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    },
+    content: {
+        type: String,
+        minlength: 10,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    bestFor: {
+        type: [String]
+    }
+})
+
+
+
+
+
+mongoose.model('Reviews', schema);
