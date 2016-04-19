420af115b16843c64b030ce443620d3ad369728f
Justin Isaf <code@justinisaf.com>
2016-04-19 12:30:51 -0400

    added an ensureAuthenticated module in a new /server/utils directory

---
+++ server/utils/ensureAuthenticated.js
@@ -0,0 +1,14 @@
+'use strict';
+// var router = require('express').Router();
+// module.exports = router;
+// var _ = require('lodash');
+
+var ensureAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(401).end();
+    }
+};
+
+module.exports = ensureAuthenticated
