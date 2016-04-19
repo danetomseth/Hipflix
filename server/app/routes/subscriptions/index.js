30cfb20a04d9888f1656acf5d18e25161b92f4fa
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2016-04-19 16:27:52 -0400

    update subscription route

--- server/app/routes/subscriptions/index.js
+++ server/app/routes/subscriptions/index.js
@@ -7,12 +7,12 @@ module.exports = router;
 
 router.get('/', (req, res, next) => {
 	Subscriptions.find({})
-	.then(subscriptions => res.send(subscriptions))
+	.then(subscriptions => res.json(subscriptions))
 	.catch(next);
 });
 
 router.post('/', (req, res, next) => {
 	Subscriptions.create(req.body)
-	.then(createdSubscription => res.send(createdSubscription))
+	.then(createdSubscription => res.json(createdSubscription))
 	.catch(next);
 });
