ea39e8470928d52a80207ed5ae2c6bfebf1e329a
danetomseth <danetomseth@gmail.com>
2016-04-19 16:09:56 -0400

    hotfix of model routes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,12 @@
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
-require('./users');
+require('./addresses');
+require('./billing');
+require('./categories');
+require('./MovieQueues');
 require('./movies');
+require('./orders');
+require('./reviews');
+require('./subscriptions');
+require('./users');
