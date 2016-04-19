dc7cf62560d9e9209b030393c4ec5d2c1c6142e0
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2016-04-19 16:24:13 -0400

    update user route

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const Users = mongoose.model('Users');
 const Reviews = mongoose.model('Reviews');
+const Orders = mongoose.model('Orders');
 module.exports = router;
 
 router.get('/', (req, res, next) => {
@@ -21,6 +22,8 @@ router.post('/', (req, res, next) => {
 router.params('/:userId', (req, res, next, id) => {
 	Users.findById(id)
 		.populate('movieQueue')
+		.populate('address')
+		.populate('billingHistory')
 		.then(user => {
 			if(!user) {
 				res.sendStatus(404);
@@ -44,5 +47,15 @@ router.get('/:userId/moviequeue', (req, res, next) => {
 router.get('/:userId/reviews', (req, res, next) => {
 	Reviews.find({user: req.user._id})
 		.then(reviewsOfOneUser => res.json(reviewsOfOneUser))
+		.catch(next);
+});
+
+router.get('/:userId/billing', (req, res, next) => {
+	res.json(req.user.billingHistory)
+});
+
+router.get('/:userId/orders', (req, res, next) => {
+	Orders.find({user: req.user._id})
+		.then(ordersOfOneUser => res.json(ordersOfOneUser))
 		.catch(next);
 });
