d180ccca53bfe772fd80567119825322e063257d
cindyliu686 <cindyliuyn@gmail.com>
2016-04-20 16:33:36 -0400

    add homepage

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,32 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller:'HomeCtrl'
     });
+});
+
+var Promise = require('bluebird')
+
+app.controller('HomeCtrl', function($scope, CategoriesFactory){
+
+	CategoriesFactory.fetchAll()
+		.then(categories => {
+			// find movies of each category
+			var categoryPromises = categories.map(category => CategoriesFactory.fetchOne(category));
+
+			Promise.all(categoryPromises)
+				.then(categoryMovies => {
+					 for(var i=0; i<categoryMovies.length;i++){
+					 	categories[i].movies = categoryMovies[i]
+					 }
+					 return categories
+				})
+			
+		})
+		.then(categories => {
+			$scope.categories = categories
+		})
+
+
 });
