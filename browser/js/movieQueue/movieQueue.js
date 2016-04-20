155656ecf4c02f86c1092e60dee3cd58e2acb1cd
cindyliu686 <cindyliuyn@gmail.com>
2016-04-20 15:43:20 -0400

    modify moviequeue

--- browser/js/movieQueue/movieQueue.js
+++ browser/js/movieQueue/movieQueue.js
@@ -1,19 +1,17 @@
 app.config(function($stateProvider) {
 	$stateProvider.state('movieQueue', {
-		url: '/:userId/moviequeue',
+		url: '/moviequeue',
 		controller: 'MovieQueueCtrl',
 		templateUrl: '/js/movieQueue/movieQueue.html',
-		resolve: {
-			user: function($stateParams) {
-				var user = new User({_id: $stateParams.id});
-				return user.fetch();        // create user factory for accessing user info
-			}
-		}
 	})
 })
 
 
 
-app.controller('MovieQueueCtrl', function($scope, user, MovieQueueFactory){
-	$scope.user = user;
+app.controller('MovieQueueCtrl', function($scope,AuthService){
+	AuthService.getLoggedInUser()
+		.then(user => {
+			$scope.user = user
+		})
+
 })
