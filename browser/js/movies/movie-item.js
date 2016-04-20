09373f4adabd4b6f1e586c03d96552e9a94d3f06
danetomseth <danetomseth@gmail.com>
2016-04-20 10:31:30 -0400

    removed isolate scope

--- browser/js/movies/movie-item.js
+++ browser/js/movies/movie-item.js
@@ -2,9 +2,7 @@ app.directive('movieItem', function(MovieFactory) {
 	return {
 		restrict: 'E',
 		templateUrl: '/js/movies/movie-item.html',
-		scope: {
-			movie: '='
-		}, 
+		
 		link: function(scope, element, attr) {
 
 		}
