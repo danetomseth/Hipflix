a7c3b41d89e5e4cf141880c586115453c4f77d2b
cindyliu686 <cindyliuyn@gmail.com>
2016-04-20 15:07:29 -0400

    update on movie-queue-item

--- browser/js/movieQueue/movie-queue-item/movie-queue-item.js
+++ browser/js/movieQueue/movie-queue-item/movie-queue-item.js
@@ -2,7 +2,6 @@ app.directive('hfMoviequeueItem', function(MovieFactory) {
 	return {
 		restrict: 'E',
 		templateUrl: '/js/movieQueue/movie-queue-item/movie-queue-item.html',
-		
 		link: function(scope, element, attr) {
 
 		}
