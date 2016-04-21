cb84391ab0945d00f0e394399771dabe4b3b52df
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2016-04-21 11:47:32 -0400

    changed

--- browser/js/reviews/reviews-controller.js
+++
@@ -1,7 +0,0 @@
-app.controller('ReviewsCtrl', function($scope, ReviewsFactory) {
-	ReviewsFactory.fetchAllByMovie()
-	.then((res) => {
-		console.log('reviews submitted', res);
-		$scope.reviews = res;
-	})
-})
