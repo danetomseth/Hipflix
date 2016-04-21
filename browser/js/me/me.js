89d27b1610c0e5d9ec5c534597d5c6580bbabfbe
cindyliu686 <cindyliuyn@gmail.com>
2016-04-21 11:36:06 -0400

    add user setting,cancal sub and billing pages

--- browser/js/me/me.js
+++ browser/js/me/me.js
@@ -1,6 +1,16 @@
 app.config(function($stateProvider) {
 	$stateProvider.state('me', {
 		url: '/me',
-		templateUrl: '/js/me/me.html'
+		templateUrl: '/js/me/me.html',
+		controller: 'MyAccountCtrl'
 	})
+})
+
+
+app.controller('MyAccountCtrl', function($scope,AuthService){
+	AuthService.getLoggedInUser()
+		.then(user => {
+			$scope.user = user
+		})
+
 })
