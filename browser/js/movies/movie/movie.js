c6a6e41d90fc921ebc8744f8cb4681825dd6c986
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2016-04-25 14:45:56 -0400

    trailer

--- browser/js/movies/movie/movie.js
+++ browser/js/movies/movie/movie.js
@@ -17,6 +17,20 @@ app.config(function($stateProvider) {
 app.controller('MovieCtrl', function($scope, $state, MovieFactory, MovieQueueFactory, AuthService, movie) {
 
 	$scope.movie = movie;
+
+	function getId() {
+	    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+	    const match = $scope.movie.trailer.match(regExp);
+
+	    if (match && match[2].length == 11) {
+	        $scope.movieTrailer = match[2];
+	        console.log('MOVIE TRAILER', $scope.movieTrailer)
+	        console.log('MOVIE TRAILER', movie.trailer)
+	    } else {
+	        return 'error';
+    	}
+	}
+
 	AuthService.getLoggedInUser()
 		.then(user => {
 			$scope.user = user
