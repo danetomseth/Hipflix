7c6ef54384eadb604de8b181c29d96e405378c67
cindyliu686 <cindyliuyn@gmail.com>
2016-04-25 17:08:34 -0400

    update

--- browser/js/subscription/subscription.js
+++ browser/js/subscription/subscription.js
@@ -14,7 +14,7 @@ app.config(function($stateProvider){
     })
 });
 
-app.controller('SubscriptionCtrl', function($scope, $state, AuthService, SubscriptionFactory){
+app.controller('SubscriptionCtrl', function($q,$scope, $state, AuthService, SubscriptionFactory){
     AuthService.getLoggedInUser().then(function (user) {
         $scope.user = user;
     });
@@ -25,12 +25,35 @@ app.controller('SubscriptionCtrl', function($scope, $state, AuthService, Subscri
         return $scope.subscriptions = subscriptions
     });
 
+     MovieQueueFactory.getWishlist()
+    .then(wishlist => {
+            console.log('get wishlist',wishlist)
+            $scope.wishList = wishlist
+    })
+
     $scope.submit = function(sub){
         if(!$scope.user) return alert("login or signup please")
             console.log($scope.user)
-        return SubscriptionFactory.update($scope.user, sub)
+        SubscriptionFactory.update($scope.user, sub)
         .then(user => {
             console.log("updated", user)
+            // if the user has already added some movies to the wishlist, add the movies in the wishlist
+            // into its movie queue.
+            if($scope.wishList.length > 0){
+                    var arrOfPromises = $scope.wishList.map(movie => {
+                        return MovieQueueFactory.addToQueue(user, movie._id)
+                    })
+                    $q.all(arrOfPromises)
+                      .then(()=>{
+                        console.log("all the movies in the wishlist into the moviequeue")
+                        return user
+                      })
+               
+            } else {
+                return user
+            }
+        })
+        .then((user) => {
             $state.go('home') // ultimately send this back to the users' billing page
         })
     }
